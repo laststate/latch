0b593e2abde31ed5c098ccea096f3a857afb748d
Matheus Henrique(Aka: TheusHen) <mmmatheushenriqueee@gmail.com>
2026-07-15 09:38:27 -0300

    style(prod): compact production sources

--- ports/rp2040/rp_reset.h
+++ ports/rp2040/rp_reset.h
@@ -2,9 +2,5 @@
 #define LASTSTATE_RP_RESET_H
 #include <stdint.h>
 #include "laststate/event.h"
-typedef struct {
-    volatile uint32_t *reason_register;
-    uint32_t watchdog_mask, forced_mask, brownout_mask, power_on_mask;
-} ls_rp_reset_port_t;
-ls_reset_info_t ls_rp_reset_info(void *context);
+typedef struct{volatile uint32_t*reason_register;uint32_t watchdog_mask,forced_mask,brownout_mask,power_on_mask;}ls_rp_reset_port_t;ls_reset_info_t ls_rp_reset_info(void*context);
 #endif
--- ports/zephyr/zephyr_can.h
+++ ports/zephyr/zephyr_can.h
@@ -3,13 +3,5 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include "laststate/transport.h"
-typedef struct {
-    ls_transport_backend_t backend;
-    const void *device;
-    uint32_t arbitration_id, timeout_ms;
-    bool can_fd, bitrate_switch;
-} ls_zephyr_can_transport_t;
-ls_result_t ls_zephyr_can_transport_init(ls_zephyr_can_transport_t *transport, const void *device,
-                                         uint32_t arbitration_id, bool can_fd, bool bitrate_switch,
-                                         uint32_t timeout_ms);
+typedef struct{ls_transport_backend_t backend;const void*device;uint32_t arbitration_id,timeout_ms;bool can_fd,bitrate_switch;}ls_zephyr_can_transport_t;ls_result_t ls_zephyr_can_transport_init(ls_zephyr_can_transport_t*transport,const void*device,uint32_t arbitration_id,bool can_fd,bool bitrate_switch,uint32_t timeout_ms);
 #endif
