7d525899499842391cd3fd1fc91ed960bc5bd7d4
Matheus Henrique(Aka: TheusHen) <mmmatheushenriqueee@gmail.com>
2026-08-08 14:04:44 -0300

    Fix embedded Linux storage initializer formatting

--- ports/embedded-linux/file_backend.h
+++ ports/embedded-linux/file_backend.h
@@ -23,7 +23,8 @@ typedef struct {
     bool initialized;
 } ls_file_storage_t;
 
-#define LS_FILE_STORAGE_INITIALIZER(path_value, size_value) {(path_value), (size_value), -1, false}
+#define LS_FILE_STORAGE_INITIALIZER(path_value, size_value) \
+    {(path_value), (size_value), -1, false}
 
 /* Create/open and preallocate an erased (0xff) backing file. Idempotent. */
 ls_result_t ls_file_storage_init(ls_file_storage_t *file);
